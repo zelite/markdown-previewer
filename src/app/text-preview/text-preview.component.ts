d3fb53788822edcfccb770219c81363a2319410e
zelite <jcafernandes@gmail.com>
2019-07-11 20:52:42 +0200

    css adjustment in rendered markdown. smaller example.

--- src/app/text-preview/text-preview.component.ts
+++ src/app/text-preview/text-preview.component.ts
@@ -1,25 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import marked from 'marked';
 
 @Component({
   selector: 'app-text-preview',
   templateUrl: './text-preview.component.html',
-  styleUrls: ['./text-preview.component.scss']
+  styleUrls: ['./text-preview.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class TextPreviewComponent implements OnInit {
+export class TextPreviewComponent{
   renderedHtml = '';
 
   @Input() set markdownText(value: string) {
     this.renderedHtml = marked(value);
   }
-
-
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    marked.setOptions({sanitize: true});
-  }
-
 }
